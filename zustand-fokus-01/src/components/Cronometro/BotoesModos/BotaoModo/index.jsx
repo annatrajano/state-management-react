0602090d3eef08051e8f6609eae9a7e1289f9c35
annatrajano <annatrajano95@gmail.com@example.com>
2025-05-27 17:44:22 -0300

    feat: :sparkles: define time mode on Buttons

--- zustand-fokus-01/src/components/Cronometro/BotoesModos/BotaoModo/index.jsx
+++ zustand-fokus-01/src/components/Cronometro/BotoesModos/BotaoModo/index.jsx
@@ -1,10 +1,23 @@
 import styles from "./styles.module.css";
+import { useCronometroStore } from "../../../../store";
+
+
 export default function BotaoModo({ children, modoBotao }) {
+  const modoCronometro = useCronometroStore((estado) => estado.modoCronometro);
+  const setModoCronometro = useCronometroStore((estado) => estado.setModoCronometro);
+
+  function aoClicar() {
+    setModoCronometro(modoBotao);
+  }
+
+  const ativo = modoBotao.id === modoCronometro.id;
+
   return (
     <button
+      onClick={aoClicar}
       className={`
         ${styles["cronometer-modes__button"]}
-        ${styles["cronometer-modes__button--active"]}
+        ${ativo ? styles["cronometer-modes__button--active"] : ""}
       `}
     >
       {children}
